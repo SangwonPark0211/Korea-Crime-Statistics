9ad70ee40622a456042cb7339b8856a4500fbd31
PSW <tkddnjs0211@gmail.com>
2021-11-10 21:53:45 +0900

    initial commit

--- csv_file_read.py
+++ csv_file_read.py
@@ -1,16 +1,23 @@
 import csv
+# data preprocessing
+read_file = open('경찰청_전국 경찰서별 강력범죄 발생 현황_20201231.csv', 'r', encoding='euc-kr')
+read_data = csv.reader(read_file)
+write_file = open('output.csv', 'w', encoding='utf-8', newline='')
+wr = csv.writer(write_file)
+wr.writerow(['Year', 'PoliceOffice', 'Total', 'Murder', 'Robber', 'Theft', 'Violence'])
+po_dict = {}
+for i, line in enumerate(read_data):
+    if i==0:    continue
+    year, po, total, m, r, t, v = line
+    if po not in po_dict:
+        po_dict[po] = len(po_dict)
+    po = po_dict[po]
+    wr.writerow([year, po, total, m, r, t, v])
+read_file.close()  
+write_file.close()
 
-# read
-f = open('경찰청_전국 경찰서별 강력범죄 발생 현황_20201231.csv', 'r', encoding='euc-kr')
-rdr = csv.reader(f)
-for line in rdr:
-    print(line)
-    break
-f.close()  
-
-# write
-f = open('output.csv', 'w', encoding='utf-8', newline='')
-wr = csv.writer(f)
-wr.writerow([1, "김정수", False])
-wr.writerow([2, "박상미", True])
-f.close()
+# police office look-up table save
+po_file = open('police_office_table.txt', 'w', encoding='euc-kr')
+for key in po_dict:
+    po_file.write(str(po_dict[key]) + ' ' + key + '\n')
+po_file.close()
